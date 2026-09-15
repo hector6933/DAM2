55b0a91d0b72b8fecda4191cc8af90f55994ec45
hector6933 <hcriadog@iesch.org>
2026-09-15 21:10:25 +0200

    Cosas de móviles y javascript

---
+++ PMD/Android/app/src/main/java/org/iesch/primerproyecto/MainActivity.kt
@@ -0,0 +1,23 @@
+package org.iesch.primerproyecto
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // println("hola que tal estamos chavales todo bien o que")
+
+        // Todo lo que queremos hacer en la pantalla empieza aquí.
+    }
+}
