512e058bd4bb4946ac11c8cc8605d428b1904748
hector6933 <hcriadog@iesch.org>
2026-09-17 18:18:50 +0200

    Más desarrollo en la app

--- PMD/EdadCanina/app/src/main/java/org/iesch/edadcanina/MainActivity.kt
+++ PMD/EdadCanina/app/src/main/java/org/iesch/edadcanina/MainActivity.kt
@@ -1,12 +1,18 @@
 package org.iesch.edadcanina
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,5 +25,32 @@ class MainActivity : AppCompatActivity() {
         // Aquí la pantalla está creada.
 
         // 1 - Tomamos el control de todos los elementos de la parte de la interfaz
+
+        val resultText = findViewById<TextView>(R.id.texto_respuesta)
+        val calculateButton = findViewById<Button>(R.id.boton_calcular)
+        val ageEdit = findViewById<EditText>(R.id.edit_edad)
+
+        // 2 - Los botones tienen la propiedad setonClickListener al pulsarlo
+
+        calculateButton.setOnClickListener {
+
+            // Aquí metemos el código de lo que queremos hacer cuando pulsemos el botón
+            // Necesitamos pasar el valor a entero
+            if (!ageEdit.text.toString().isEmpty()) {
+
+                val edadInt = ageEdit.text.toString().toInt()
+
+                val dogAge = edadInt*7
+                // resultText.text = "Si fueras un perro tendrías $dogAge años"
+                resultText.text = getString(R.string.texto_respuesta, dogAge)
+
+            } else {
+
+                Toast.makeText(this, "Introduce algo, hijo de puta!", Toast.LENGTH_LONG).show()
+
+            }
+
+        }
+
     }
 }
